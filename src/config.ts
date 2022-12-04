065249831e30cb6677a748a3a60f7a0b2593558e
KakaoStream <kakaostream@gmail.com>
2022-12-05 00:25:37 +0100

    init commit

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Kakao Productions';
+export const SITE_DESCRIPTION = 'Willkommen auf meiner Webseite';
