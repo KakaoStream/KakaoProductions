29842d5c8d99569ecd5107a98d6fa08bee01a5bd
DevTobias <tobi.kaerst@gmx.de>
2022-12-05 18:25:05 +0100

    feature: refactored code structure

---
+++ frontend/src/config.ts
@@ -0,0 +1,9 @@
+export const SITE = {
+  origin: "https://www.example.com",
+  basePathname: "/",
+
+  title: "Kakao Productions",
+  description: "Willkommen auf meiner Webseite",
+
+  language: "de",
+};
